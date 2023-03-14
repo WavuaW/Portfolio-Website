e14dacbe346c5d09893a10cbc11204f506cb3dae
dennisfofie <dennisbeckham000@gmail.com>
2023-03-14 17:59:37 +0000

    add permission

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,11 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class ShipmentReadOnly(BasePermission):
-
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
-    
 
 class ShipmentCustomerOrReadOnly(BasePermission):
     
