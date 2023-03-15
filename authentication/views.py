c79913cfb0c79a3b818afdca316e3c0a6bccb996
Your Name <you@example.com>
2023-03-15 02:24:04 +0100

    added some functionalities in the authentication

--- authentication/views.py
+++ authentication/views.py
@@ -4,8 +4,8 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-#from rest_framework.authentication import TokenAuthentication
-#from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 from .models import Shipment, ShippingTo, ShippingFrom
@@ -18,8 +18,8 @@ class ShipmentListView(APIView):
     """
     List all shipment, or create a new shipment
     """
-    #authentication_classes=[TokenAuthentication]
-    #permission_classes=[IsAuthenticated]
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
 
   
     def get(self, request, format=None):
