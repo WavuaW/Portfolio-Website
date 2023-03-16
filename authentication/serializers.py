1c5b3a05f159c79a22e443c1482db794b07cb6c3
dennisfofie <dennisbeckham000@gmail.com>
2023-03-16 11:29:36 +0000

    implement

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,9 +4,15 @@ from .models import Shipment, ShippingTo, ShippingFrom
 
 
 class ShipmentSerializers(serializers.ModelSerializer):
+
     class Meta:
         model = Shipment
         fields = '__all__'
+
+    def create(self, validated_data):
+        shipment = Shipment.objects.create(**validated_data)
+        shipment.save()
+        return shipment
 
 
 class ShippingToSerializers(serializers.ModelSerializer):
@@ -14,8 +20,19 @@ class ShippingToSerializers(serializers.ModelSerializer):
         model = ShippingTo
         fields = '__all__'
 
+    def create(self, validated_data):
+        shipment = Shipment.objects.create(**validated_data)
+        shipment.save(self)
+        return shipment
+    
+    
 
 class ShippingFromSerializers(serializers.ModelSerializer):
     class Meta:
         model = ShippingFrom
         fields = '__all__'
+
+    def create(self, validated_data):
+        shipment = Shipment.objects.create(**validated_data)
+        shipment.save(self)
+        return shipment
